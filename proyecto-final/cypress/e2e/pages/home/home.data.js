f846184817c707afe53de50f674cf6b143e107fe
adriel <adrielc.dev@gmail.com>
2025-08-04 10:25:41 -0300

    Creación de archivos de HomePage

---
+++ proyecto-final/cypress/e2e/pages/home/home.data.js
@@ -0,0 +1,9 @@
+export class HomeData {
+    static get phone() {
+        return {
+            sansungGalaxyS6: 'Samsung galaxy s6',
+            nokiaLumia1520: 'Nokia lumia 1520',
+            nexus6: 'Nexus 6'
+        }
+    }
+}
--- proyecto-final/cypress/e2e/spec.cy.js
+++ proyecto-final/cypress/e2e/spec.cy.js
@@ -1,16 +1,25 @@
+import { HomeMethods } from "./pages/home/home.methods";
 import { LoginMethods } from "./pages/login/login.methods";
 
 describe('template spec', () => {
   it('passes', () => {
-    const usuario = 'random01';
-    const password = 'random01';
-    
     cy.visit('https://www.demoblaze.com');
-    cy.get('a[data-target="#logInModal"]').click();
-    LoginMethods.login(usuario, password);
-    cy.get('a#nameofuser').should('contain.text', usuario);
+    HomeMethods.clickOnProductLink('Iphone 6 32gb');
+    cy.wait(20000);
     
-  })
+  });
+  
+  // it('passes', () => {
+  //   const usuario = 'random01';
+  //   const password = 'random01';
+    
+  //   cy.visit('https://www.demoblaze.com');
+  //   cy.get('a[data-target="#logInModal"]').click();
+  //   LoginMethods.login(usuario, password);
+  //   cy.get('a#nameofuser').should('contain.text', usuario);
+    
+  // });
+
   // it('passes', () => {
   //   cy.visit('https://www.demoblaze.com')
   //   cy.get('a[data-target="#logInModal"]').click();
