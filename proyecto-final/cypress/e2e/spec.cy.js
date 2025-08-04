339ee86881f66949897b7e61246e56b2b997e445
adriel <adrielc.dev@gmail.com>
2025-08-04 09:37:14 -0300

    Login Page

--- proyecto-final/cypress/e2e/spec.cy.js
+++ proyecto-final/cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.demoblaze.com')
+    cy.get('a[data-target="#logInModal"]').click();
+    LoginMethods.login('username', 'password');
+    cy.wait(5000);
   })
 })
