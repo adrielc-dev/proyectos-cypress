5c50b1aefb038c28881bef0f257484b4c5ceaba1
adriel <adrielc.dev@gmail.com>
2025-08-04 09:58:42 -0300

    Fix report and flakyness

--- proyecto-final/cypress.config.js
+++ proyecto-final/cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
+    retries: 1,
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
--- proyecto-final/cypress/e2e/spec.cy.js
+++ proyecto-final/cypress/e2e/spec.cy.js
@@ -2,9 +2,19 @@ import { LoginMethods } from "./pages/login/login.methods";
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.demoblaze.com')
+    const usuario = 'random01';
+    const password = 'random01';
+    
+    cy.visit('https://www.demoblaze.com');
     cy.get('a[data-target="#logInModal"]').click();
-    LoginMethods.login('username', 'password');
-    cy.wait(5000);
+    LoginMethods.login(usuario, password);
+    cy.get('a#nameofuser').should('contain.text', usuario);
+    
   })
+  // it('passes', () => {
+  //   cy.visit('https://www.demoblaze.com')
+  //   cy.get('a[data-target="#logInModal"]').click();
+  //   LoginMethods.login('username', 'password');
+  //   cy.wait(5000);
+  // })
 })
