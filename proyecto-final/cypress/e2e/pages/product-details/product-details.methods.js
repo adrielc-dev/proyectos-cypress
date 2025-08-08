3f44df52729651f14c6fe8f5f3840c6adf285143
adriel <adrielc.dev@gmail.com>
2025-08-08 18:51:07 -0300

    Creación de archivo de Product Details

---
+++ proyecto-final/cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { ProductDetailElements } from "./product-details.elements";
+
+export class ProductDetailMethods{
+    static clickOnAddToCartButton(){
+        ProductDetailElements.buttons.addToCart.click();
+    }
+}
